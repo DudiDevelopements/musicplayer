4fd39760454dbd95c878fbf1d65549008a73fda7
DudiDevelopements <48138200+DudiDevelopements@users.noreply.github.com>
2020-02-24 22:10:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Importa o kivy:
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
 # Importa o necessario para reproduzir o som:
@@ -10,8 +11,10 @@ import os
 # Inicializa o pygame
 pygame.init()
 
+music_path = ''
 
-def play_music(music_path):
+
+def play_music():
     pygame.mixer.music.load(music_path)
     pygame.mixer.music.play()
     pygame.mixer.music.set_volume(1)
@@ -24,6 +27,10 @@ def stop_music():
     pygame.mixer.music.stop()
 
 
+class TextInput(Widget):
+    pass
+
+
 class RootWidget(FloatLayout):
     pass
 
@@ -32,6 +39,10 @@ class PlayApp(App):
 
     def build(self):
         return RootWidget()
+
+    def printPath(self):
+        music_path = self.root.ids.pathinput.text
+        print(music_path)
 
 
 PlayApp().run()
